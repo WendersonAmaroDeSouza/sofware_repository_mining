3e59f8a86276e268245383371ea49da0dcf63cf8
Wenderson Amaro De Souza <wenderson3souza@gmail.com>
2022-07-02 18:21:22 -0400

    all files

--- extract_commits.py
+++ extract_commits.py
@@ -2,8 +2,11 @@ from pydriller import Repository
 import pandas as pd
 from repositories import list_all_repositories_by_category
 from commits import get_commit_metadatas
+from group_repository_metadatas import get_filtred_repository_metadatas
 
 categories = ['100_a_500', '500_a_1000', '1000_a_5000', '50000_a_100000', '100000_a_200000']
+
+df_repositories_dataset = pd.DataFrame()
 
 for category in categories:
     df_repositories_metadatas = pd.DataFrame()
@@ -16,5 +19,8 @@ for category in categories:
         df_repository_metadatas = pd.DataFrame(commits_metadatas)
         df_repository_metadatas['repository_category'] = category
         df_repositories_metadatas = pd.concat([df_repositories_metadatas, df_repository_metadatas])
-    df_repositories_metadatas.to_csv('repositories.csv', sep=';', index=False)
-    exit()
+        print(df_repositories_metadatas['project_name'][0])
+    df_repositories_dataset = pd.concat([df_repositories_dataset, get_filtred_repository_metadatas(df_repositories_metadatas)])
+    print(category)
+
+df_repositories_dataset.to_csv('repositories_dataset.csv', sep=';', index=False)
--- group_repository_metadatas.py
+++ group_repository_metadatas.py
@@ -1,26 +1,25 @@
 import pandas as pd
 from datetime import datetime
 
-df_repository_metadatas = pd.read_csv('repositories.csv', sep=';')
+def get_filtred_repository_metadatas(df_repository_metadatas):
+    df_repository_metadatas = df_repository_metadatas[['repository_category', 'project_name', 'author_name', 'author_date', 'dmm_unit_complexity', 'complexity']]
+    df_repository_metadatas = df_repository_metadatas[~df_repository_metadatas['dmm_unit_complexity'].isna()]
+    df_repository_metadatas['year'] = df_repository_metadatas['author_date'].apply(lambda x : x.year )
+    df_repository_metadatas['month'] = df_repository_metadatas['author_date'].apply(lambda x : x.month )
+    df_repository_metadatas = df_repository_metadatas.drop(columns='author_date')
 
-df_repository_metadatas = df_repository_metadatas[['repository_category', 'project_name', 'author_name', 'author_date', 'dmm_unit_complexity', 'complexity']]
-df_repository_metadatas = df_repository_metadatas[~df_repository_metadatas['dmm_unit_complexity'].isna()]
-df_repository_metadatas['year'] = df_repository_metadatas['author_date'].apply(lambda x : datetime.fromisoformat(x).year )
-df_repository_metadatas['month'] = df_repository_metadatas['author_date'].apply(lambda x : datetime.fromisoformat(x).month )
-df_repository_metadatas = df_repository_metadatas.drop(columns='author_date')
+    df_repository_metadatas = df_repository_metadatas.groupby(by=['year', 'month', 'project_name', 'repository_category'], as_index=False).agg({
+        'author_name': pd.Series.unique,
+        'dmm_unit_complexity': ['mean'],
+        'complexity': ['mean']
+    })
 
-df_repository_metadatas = df_repository_metadatas.groupby(by=['year', 'month', 'project_name', 'repository_category'], as_index=False).agg({
-    'author_name': pd.Series.unique,
-    'dmm_unit_complexity': ['mean'],
-    'complexity': ['mean']
-})
+    df_repository_metadatas = df_repository_metadatas.reset_index()
 
-df_repository_metadatas = df_repository_metadatas.reset_index()
+    df_repository_metadatas['contribuitors_number'] = df_repository_metadatas['author_name']['unique'].apply(lambda x : len(x))
+    df_repository_metadatas['dmm_unit_complexity_value'] = df_repository_metadatas['dmm_unit_complexity']['mean'].apply(lambda x : x )
+    df_repository_metadatas['complexity_value'] = df_repository_metadatas['complexity']['mean'].apply(lambda x : x )
 
-df_repository_metadatas['contribuitors_number'] = df_repository_metadatas['author_name']['unique'].apply(lambda x : len(x))
-df_repository_metadatas['dmm_unit_complexity_value'] = df_repository_metadatas['dmm_unit_complexity']['mean'].apply(lambda x : x )
-df_repository_metadatas['complexity_value'] = df_repository_metadatas['complexity']['mean'].apply(lambda x : x )
+    df_repository_metadatas = df_repository_metadatas[['year', 'month', 'repository_category', 'project_name', 'dmm_unit_complexity_value', 'complexity_value', 'contribuitors_number']].sort_values(by=['project_name', 'year', 'month'])
 
-df_repository_metadatas = df_repository_metadatas[['year', 'month', 'repository_category', 'project_name', 'dmm_unit_complexity_value', 'complexity_value', 'contribuitors_number']]
-
-print(df_repository_metadatas)
+    return df_repository_metadatas
