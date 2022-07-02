7bdf63556b2fd3737e95d41150b067dd537bfa49
Juliano Carvalho <julianonecar@gmail.com>
2022-07-01 21:23:47 -0400

    Repositories URLs

--- repositories.py
+++ repositories.py
@@ -1,4 +1,29 @@
+#Repos 100 a 500 stars
 software_repositories_paths = [
- 'path': './',
- 'https://github.com/public-apis/public-apis.git'
+    'path': './',
+    'https://github.com/taki0112/Densenet-Tensorflow', 'https://github.com/longcw/MOTDT', 'https://github.com/xiaoshun007/12306Python', 'https://github.com/OrkoHunter/keep'
+]
+
+#Repos 500 a 1000 stars
+software_repositories_paths2 = [
+    'path': './',
+    'https://github.com/clovaai/CutMix-PyTorch', 'https://github.com/CacheBrowser/cachebrowser', 'https://github.com/kumar-shridhar/PyTorch-BayesianCNN', 'https://github.com/mushorg/conpot' 
+]
+
+#Repos 1000 a 5000 stars
+software_repositories_paths3 = [
+    'path': './',
+    'https://github.com/psf/requests', 'https://github.com/tiangolo/fastapi', 'https://github.com/python/cpython', 'https://github.com/3b1b/manim'
+]
+
+#Repos 50000 a 100000 stars
+software_repositories_paths4 = [
+    'path': './',
+    'https://github.com/tensorflow/models', 'https://github.com/nvbn/thefuck', 'https://github.com/huggingface/transformers', 'https://github.com/django/django'
+]
+
+#Repos 100000 a 200000 stars
+software_repositories_paths5 = [
+    'path': './',
+    'https://github.com/public-apis/public-apis.git', 'https://github.com/donnemartin/system-design-primer', 'https://github.com/TheAlgorithms/Python', 'https://github.com/vinta/awesome-python'
 ]
