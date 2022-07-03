27559ea2ed1c9d31301045263cd256dbcffcfca6
Wenderson Amaro De Souza <wenderson3souza@gmail.com>
2022-07-02 20:16:54 -0400

    all files

--- extract_commits.py
+++ extract_commits.py
@@ -4,12 +4,11 @@ from repositories import list_all_repositories_by_category
 from commits import get_commit_metadatas
 from group_repository_metadatas import get_filtred_repository_metadatas
 
-categories = ['100_a_500', '500_a_1000', '1000_a_5000', '50000_a_100000', '100000_a_200000']
+categories = ['100_a_500', '500_a_1000', '1000_a_5000'] # , '50000_a_100000', '100000_a_200000']
 
 df_repositories_dataset = pd.DataFrame()
 
 for category in categories:
-    df_repositories_metadatas = pd.DataFrame()
     repositories = list_all_repositories_by_category(category)
     for repository in repositories:
         commits_metadatas = []
@@ -18,9 +17,6 @@ for category in categories:
             commits_metadatas.append(get_commit_metadatas(commit))
         df_repository_metadatas = pd.DataFrame(commits_metadatas)
         df_repository_metadatas['repository_category'] = category
-        df_repositories_metadatas = pd.concat([df_repositories_metadatas, df_repository_metadatas])
-        print(df_repositories_metadatas['project_name'][0])
-    df_repositories_dataset = pd.concat([df_repositories_dataset, get_filtred_repository_metadatas(df_repositories_metadatas)])
+        df_repositories_dataset = pd.concat([df_repositories_dataset, get_filtred_repository_metadatas(df_repository_metadatas)])
+        df_repositories_dataset.to_csv('repositories_dataset.csv', sep=';', index=False)
     print(category)
-
-df_repositories_dataset.to_csv('repositories_dataset.csv', sep=';', index=False)
